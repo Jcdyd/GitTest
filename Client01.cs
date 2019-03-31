71d3c5b1fe06670a705ceccc1aea87abc2277210
Jcdyd <48319435+Jcdyd@users.noreply.github.com>
2019-03-31 22:06:48 +0800

    第一次远程段修改代码
    第一次远程段修改这个客户端代码

--- Client01.cs
+++ Client01.cs
@@ -9,6 +9,7 @@ using System.Threading;
 public class Client01 : MonoBehaviour {
 
     //定义ip、端口 字段...
+	//远程段修改代码
     private string ip = "192.168.0.103";
     private int port = 33333;
     private Socket socket;
