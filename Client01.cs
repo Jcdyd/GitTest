16242bbee44ca5e83ac6c8657b94283f62157ed0
1121438750 <1121438750@qq.com>
2019-03-31 22:19:55 +0800

    本地第二次修改，根据历史记录

--- Client01.cs
+++ Client01.cs
@@ -9,8 +9,7 @@ using System.Threading;
 public class Client01 : MonoBehaviour {
 
     //定义ip、端口 字段...
-    //本地第一次修改客户端代码
-    //远程段修改代码
+    //本地第二次修改客户端代码
     private string ip = "192.168.0.103";
     private int port = 33333;
     private Socket socket;
